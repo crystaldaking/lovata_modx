d28cc4ed36f4dbebe3f9ba34e014f51e59e3b0d9
Павел Бигель <crystal@MacBook-Air-Pavel.local>
2019-12-03 20:09:17 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+from telethon import TelegramClient
+from telethon.tl.functions.contacts import ResolveUsernameRequest
+from telethon.tl.functions.channels import GetMessagesRequest
+from telethon.tl.functions.messages import GetHistoryRequest, ReadHistoryRequest
+from telethon.tl.types import InputPeerChannel
+
